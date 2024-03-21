9718bbf52dad54e57ce72efba415bbaab3e850ad
Tyler <163706041+SirJumbly@users.noreply.github.com>
2024-03-20 23:49:02 -0400

    Console Application for C# Data Types and Concatenation
    First console application

---
+++ Concatonation Console Application/VS_Files/CSharpBasics/CSharpBasics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata;
+
+namespace CSharpBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameFull = "Jimmy Jaskins";
+            int age = 55;
+            double weight = 175.5;
+            int heightFt = 5;
+            int heightInch = 11;
+
+
+            Console.WriteLine("User statistics");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Name" + "                " + "Age" + "    " + "Height"+ "    " + "Weight");
+            Console.WriteLine(nameFull + "      " + age + "     " + heightFt+"'"+heightInch+"\"" + "     " + weight+"lbs");
+
+            Console.ReadLine();
+            
+        }
+    }
+}
